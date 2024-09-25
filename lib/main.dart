9c02fa426abc222e32702c59153e070b03297fe8
RJV-YF <freeworld.creator01@gmail.com>
2024-09-25 12:49:56 +0530

    The counter app UI with it's color changing feature  with respect to whether the counter number is odd or even is done

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,63 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+// bool isEven(int number) {
+//   if (number % 2 == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+bool isEven(int number) => number % 2 == 0 ? true : false;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+  Color color = Colors.red;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      // if (isEven(_counter)) {
+      //   color = Colors.red;
+      // } else {
+      //   color = Colors.green;
+      // }
+      isEven(_counter) ? color = Colors.red : color = Colors.green;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'You have pressed the button $_counter times',
+          style: TextStyle(
+            fontSize: 16,
+            color: color,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
 }
