c9daf351bc8156de17b850fa16b2ce50b092e28b
RJV-YF <freeworld.creator01@gmail.com>
2024-09-25 13:16:20 +0530

    Wrote seperate tests to check whether the counter is odd or even, and grouped the tests under group method.

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,13 +2,48 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_testing/main.dart';
 
 void main() {
-  test(
-    'Is Even',
+  // test(
+  //   'Is Even',
+  //   () {
+  //     bool result = isEven(12);
+  //     expect(result, true);
+  //     result = isEven(123);
+  //     expect(result, false);
+  //   },
+  // );
+
+  // test(
+  //   'Is Odd',
+  //   () {
+  //     bool result = isEven(121);
+  //     expect(result, true);
+  //     result = isEven(18);
+  //     expect(result, false);
+  //   },
+  // );
+
+  group(
+    'Is Even Group',
     () {
-      bool result = isEven(12);
-      expect(result, true);
-      result = isEven(123);
-      expect(result, false);
+      test(
+        'Is Even',
+        () {
+          bool result = isEven(12);
+          expect(result, true);
+          result = isEven(124);
+          expect(result, true);
+        },
+      );
+
+      test(
+        'Is Odd',
+        () {
+          bool result = isEven(121);
+          expect(result, false);
+          result = isEven(123);
+          expect(result, false);
+        },
+      );
     },
   );
 }
